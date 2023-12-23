10169b2bea8bb301183be67a69e779724356df05
nooblette <min9hyuk@gmail.com>
2023-12-23 18:16:25 +0900

    3. Fixture 메소드

--- src/test/java/myjunit/TestCaseTest.java
+++ src/test/java/myjunit/TestCaseTest.java
@@ -8,12 +8,20 @@ public class TestCaseTest extends TestCase {
 		super(testCaseName); // 부모 객체인 TestCase의 생성자 호출
 	}
 
+	private static long base;
+
+	public void before(){
+		base = 10;
+	}
+
 	public void runPlusTest() {
+		before(); // 각각의 테스트 메서드들이 테스트 전에 before() Fixture 메서드를 수행
 		long sum = 10 + 10;
 		Assert.assertTrue(sum == 20);
 	}
 
 	public void runMinusTest() {
+		before();
 		long sum = 100 - 10;
 		Assert.assertTrue(sum == 90);
 	}
