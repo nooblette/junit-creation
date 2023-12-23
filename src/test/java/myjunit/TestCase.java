03dc7134339fd3b11ad0fc5f5132d93c35569013
nooblette <min9hyuk@gmail.com>
2023-12-23 17:39:32 +0900

    2. TestCase - run() 추상 메서드 리팩토링

--- src/test/java/myjunit/TestCase.java
+++ src/test/java/myjunit/TestCase.java
@@ -1,5 +1,10 @@
 package myjunit;
 
+import java.lang.reflect.Method;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /*
 * 테스트 프레임워크로써 기능을 수행하기 위해, 각각 테스트 케이스 단위로 요청을 나눌 수 있도록 구성(Command Pattern)
 * 	각각의 테스트 케이스를 Command 로 보고, run 메서드가 이러한 테스트 케이스 실행을 담당한다.
@@ -7,6 +12,8 @@ package myjunit;
 * 	이를 통해 각각의 테스트는 새로운 인스턴스에서 독립적으로 수행된다.
 */
 public abstract class TestCase {
+	private static final Logger logger = LoggerFactory.getLogger(TestCaseTest.class);
+
 	// 각 TestCase를 식별하기 위한 이름, 생성자를 통해 받는다.
 	protected String testCaseName;
 
@@ -14,5 +21,13 @@ public abstract class TestCase {
 		this.testCaseName = testCaseName;
 	}
 
-	public abstract void run();
+	public void run(){
+		try{
+			logger.info("{} execute", testCaseName);
+			Method method = this.getClass().getMethod(testCaseName, null);
+			method.invoke(this, null);
+		} catch (Exception e){
+			throw new RuntimeException(e);
+		}
+	}
 }
--- src/test/java/myjunit/TestCaseTest.java
+++ src/test/java/myjunit/TestCaseTest.java
@@ -1,30 +1,12 @@
 package myjunit;
 
-import java.lang.reflect.Method;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import myjunit.assertion.Assert;
 
 public class TestCaseTest extends TestCase {
-	private static final Logger logger = LoggerFactory.getLogger(TestCaseTest.class);
 
 	public TestCaseTest(String testCaseName){
 		super(testCaseName); // 부모 객체인 TestCase의 생성자 호출
 	}
-
-	@Override
-	public void run(){
-		try{
-			logger.info("{} execute", testCaseName);
-			Method method = this.getClass().getMethod(super.testCaseName, null);
-			method.invoke(this, null);
-		} catch (Exception e){
-			throw new RuntimeException(e);
-		}
-	}
-
 
 	public void runPlusTest() {
 		long sum = 10 + 10;
